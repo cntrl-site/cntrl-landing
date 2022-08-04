ffb79228b97ff55d3b3a17a148b932ed885796a2
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-04 18:59:49 +0300

    testing mailchimp

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -1,4 +1,10 @@
 import { Handler } from '@netlify/functions'
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+mailchimp.setConfig({
+  apiKey: '0f878ada0fad58181dabfbe87a6b309f-us13',
+  server: 'us13',
+});
 
 export const handler: Handler = async (event, context) => {
   const { name = 'stranger' } = event.queryStringParameters!;
@@ -6,7 +12,7 @@ export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: mailchimp.lists.getAllLists(),
     }),
   };
 }
