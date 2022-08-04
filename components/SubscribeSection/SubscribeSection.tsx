d46eb8ae5523cb1e75c85a653306566c273e8600
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-04 21:05:05 +0300

    bring back old headers

--- components/SubscribeSection/SubscribeSection.tsx
+++ components/SubscribeSection/SubscribeSection.tsx
@@ -21,7 +21,9 @@ export const SubscribeSection: FC<Props> = ({ layouts }) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
       },
       body: JSON.stringify({ email, portfolio })
     });
