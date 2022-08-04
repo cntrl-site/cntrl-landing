5f00690584cdb8fab41bde4fb7e9ba358b78281d
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-04 21:25:49 +0300

    unsafe url

--- components/SubscribeSection/SubscribeSection.tsx
+++ components/SubscribeSection/SubscribeSection.tsx
@@ -25,6 +25,7 @@ export const SubscribeSection: FC<Props> = ({ layouts }) => {
         'Access-Control-Allow-Headers': 'Content-Type',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTION'
       },
+      referrerPolicy: 'unsafe-url'
       body: JSON.stringify({ email, portfolio })
     });
   };
