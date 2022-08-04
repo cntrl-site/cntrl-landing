ae85f7641a7472b4a406e1bf7cf540d322dae050
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-04 19:03:33 +0300

    import mailchimp

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -1,5 +1,5 @@
 import { Handler } from '@netlify/functions'
-const mailchimp = require('@mailchimp/mailchimp_marketing');
+import mailchimp from '@mailchimp/mailchimp_marketing';
 
 mailchimp.setConfig({
   apiKey: '0f878ada0fad58181dabfbe87a6b309f-us13',
@@ -7,11 +7,12 @@ mailchimp.setConfig({
 });
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters!;
+  const { email = 'stranger' } = event.queryStringParameters!;
 
   return {
     statusCode: 200,
     body: JSON.stringify({
+      // @ts-ignore
       message: mailchimp.lists.getAllLists(),
     }),
   };
