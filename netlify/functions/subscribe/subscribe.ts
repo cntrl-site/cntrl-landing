28c0eed2d2f95c79f6111c4fbe0cf53b943fbad0
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-05 11:27:35 +0300

    testing with specific origin

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -23,7 +23,7 @@ export const handler: Handler = async (event) => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "https://cntrl.site",
       "Access-Control-Allow-Headers": "Content-Type",
       "Access-Control-Allow-Methods": "GET, POST, OPTION"
     },
