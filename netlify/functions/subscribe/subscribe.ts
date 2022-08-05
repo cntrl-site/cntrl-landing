c7f6608dc87558f76cf58ccbc56789679279635e
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-08-05 11:46:12 +0300

    split into httpMethod

--- netlify/functions/subscribe/subscribe.ts
+++ netlify/functions/subscribe/subscribe.ts
@@ -4,30 +4,40 @@ import Airtable from 'airtable';
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY! }).base(process.env.AIRTABLE_BASE!);
 
 export const handler: Handler = async (event) => {
-  const body = JSON.parse(event.body!);
+  if (event.httpMethod === 'POST') {
+    const body = JSON.parse(event.body!);
 
-  await base('Early Adopters').create([
-    {
-      fields: {
-        'Email': body?.email,
-        'Portfolio': body?.portfolio
+    await base('Early Adopters').create([
+      {
+        fields: {
+          'Email': body?.email,
+          'Portfolio': body?.portfolio
+        }
       }
-    }
-  ], (err: any) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
+    ], (err: any) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "https://cntrl.site",
-      "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTION"
-    },
-    body: JSON.stringify({
-      message: 'success'
-    }),
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "https://cntrl.site",
+        "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTION"
+      },
+      body: JSON.stringify({
+        message: 'success'
+      })
+    };
+  } else {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "https://cntrl.site",
+        "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTION"
+      }
+    };
+  }
 }
